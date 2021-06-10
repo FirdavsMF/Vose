deee75a4c6e6fc50e3c522d9490d622fd26161d7
Firdavs <firdavs.2001.fed@gmail.com>
2021-06-10 15:31:05 +0500

    ...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import './css/fontawesome';
+
 import App  from './Selection/App.js'
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+ReactDOM.render(<App/>, document.getElementById('root'))
